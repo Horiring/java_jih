bf169d243903b083177eb6e5fa93737b0dfc02dd
Horiring <124111575+Horiring@users.noreply.github.com>
2023-05-31 17:27:47 +0900

    homework

--- java/src/day2/homwork/ScannerEx2.java
+++ java/src/day2/homwork/ScannerEx2.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class ScannerEx2 {
 
 	public static void main(String[] args) {
+		/*
+두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님.
+
+예:
+두 정수를 입력하세요.
+1 2 [엔터]
+1+2 =3
+1 - 2= -1
+1  * 2 = 2
+1 / 2 = 0.5
+1 % 2 = 1
+
+		 */
 		Scanner sc = new Scanner(System.in);
 		System.out.println("두 정수를 입력해주세요.");
 		int num1 = sc.nextInt(),num2 = sc.nextInt();
-		System.out.println("연산자를 입력하세여!");
-		sc.nextLine();
-		String str1 = sc.nextLine(); 
-		System.out.println(num1+str1+num2+"=");
-	}
+		System.out.println(num1+"+"+num2+"="+(num1+num2));
+		System.out.println(num1+"-"+num2+"="+(num1-num2));
+		System.out.println(num1+"*"+num2+"="+(num1*num2));
+		System.out.println(num1+"/"+num2+"="+((double)num1/num2));
+		System.out.println(num1+"%"+num2+"="+(num1%num2));
+		}
 
 }
