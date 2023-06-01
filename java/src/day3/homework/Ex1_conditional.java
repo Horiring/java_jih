f0304982e0449dad0813a0e33a893af6b67720f2
Horiring <124111575+Horiring@users.noreply.github.com>
2023-06-01 15:14:59 +0900

    Update Ex1_conditional.java

--- java/src/day3/homework/Ex1_conditional.java
+++ java/src/day3/homework/Ex1_conditional.java
@@ -8,12 +8,15 @@ public class Ex1_conditional {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input gender : ");
 		char gender = sc.next().charAt(0); 
-		boolean result = gender=='W' ? true : false; 
+		boolean result = gender=='W' ? true : false;
+		System.out.println("are you a women? "+result);
+		/*
 		if(gender=='W') {
 			System.out.println("are you a women? "+result);
 		}
 		if(gender=='M') {
 			System.out.println("are you a women? "+result);
 		}
+		*/
 	}
 }
