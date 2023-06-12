7d847927de9816daef3104f57a7514538c5e883f
Horiring <124111575+Horiring@users.noreply.github.com>
2023-06-12 10:36:33 +0900

    day8 homework

--- java/src/day8/homework/MethodArithmeticEx.java
+++ java/src/day8/homework/MethodArithmeticEx.java
@@ -2,27 +2,31 @@ package day8.homework;
 
 import java.util.Scanner;
 
+import javax.management.RuntimeErrorException;
+
 public class MethodArithmeticEx {
 
 	public static void main(String[] args) {
 		// 두 정수와 산술 연산자를 입력받아 산술 연산 결과를 출력하는 코드를 작성하세요.
 		// 연산자를 잘못 입력한 경우는 없다고 가정 , 0으로 나누는 일은 없다고 가정.
 		Scanner sc = new Scanner(System.in);
-		System.out.println("두 정수와 산술연산자를 입력해주세요. (ex :1+1)");
+		System.out.println("두 정수와 산술연산자를 입력해주세요. (ex :1 + 1)");
 		int num1 = sc.nextInt();
 		char ch = sc.next().charAt(0);
 		int num2 = sc.nextInt();
-
-		System.out.println("" + num1 + ch + num2 + "=" + ari(num1, num2, ch));
+		
+		double res = ari(num1, ch, num2);
+		System.out.println("" + num1 + ch + num2 + "=" + res);
 		sc.close();
 	}
 
-	public static double ari(int num1, int num2, char ch1) {
-		int result = 0;
+	public static double ari(int num1, char ch1, int num2) {
+		double result = 0.0;
 		switch (ch1) {
 		case '+':
 			result = num1 + num2;
 			break;
+			//return num1+num2;
 		case '-':
 			result = num1 - num2;
 			break;
@@ -30,13 +34,14 @@ public class MethodArithmeticEx {
 			result = num1 * num2;
 			break;
 		case '/':
-			result = num1 / num2;
+			result = (double)num1 / num2;
 			break;
 		case '%':
 			result = num1 % num2;
 			break;
 		default:
-			result = 0;
+			//잘못된 연산자인 경우 예외처리
+			//throw new RuntimeException("잘못된 연산자입니다.");
 		}
 		return result;
 	}
--- java/src/day8/homework/MethodMonthEx.java
+++ java/src/day8/homework/MethodMonthEx.java
@@ -5,27 +5,55 @@ import java.util.Scanner;
 public class MethodMonthEx {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("월을 입력하세요.");
-		int month = sc.nextInt();
-
-		String season = getMonth(month);
-		System.out.println(month + " is " + month);
-
-		System.out.println(month + " is " + getMonth(month));
-
-		sc.close();
-
+		/* 월을 입력받아 월에 맞는 영어를 출력하세요.
+		 * */
+		printMonth(1);
+		System.out.println(getMonth(1));
 	}
-
-	public static String getMonth (int month) {
-		String eng=" ";
+	
+	/**월이 주어지면 주어진 월의 영문을 출력하는 메서드
+	 * 매개변수 : 월 => int month
+	 * 리턴타입 : 없음 => void
+	 * 메서드명 : printMonth
+	 */
+	public static void printMonth(int month) {
 		switch(month) {
-		case 1:
-			eng="january";
-			break;
-		default:
+		case 1:	System.out.println("January"); 		break;
+		case 2:	System.out.println("February");		break;
+		case 3:	System.out.println("March"); 		break;
+		case 4:	System.out.println("April"); 		break;
+		case 5:	System.out.println("May"); 			break;
+		case 6:	System.out.println("June"); 		break;
+		case 7:	System.out.println("July"); 		break;
+		case 8:	System.out.println("August"); 		break;
+		case 9:	System.out.println("September"); 	break;
+		case 10:System.out.println("October"); 		break;
+		case 11:System.out.println("November"); 	break;
+		case 12:System.out.println("December"); 	break;
+		default:System.out.println("Wrong month!");
 		}
-		return eng;
+	}
+	
+	/**월이 주어지면 주어진 월의 영문을 알려주 메서드
+	 * 매개변수 : 월 => int month
+	 * 리턴타입 : 월의 영문 => String
+	 * 메서드명 : getMonth
+	 */
+	public static String getMonth(int month) {
+		switch(month) {
+		case 1:	return "January";
+		case 2:	return "February";
+		case 3:	return "March";
+		case 4:	return "April";
+		case 5:	return "May";
+		case 6:	return "June";
+		case 7:	return "July";
+		case 8:	return "August";
+		case 9:	return "September";
+		case 10:return "October";
+		case 11:return "November";
+		case 12:return "December";
+		default:return "Wrong month!";
+		}
 	}
 }
